2aa80a0be0818b12d6cb9c80c1ce5e2569ebc3cc
amblessed <hollazyz85@gmail.com>
2018-09-08 16:01:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>snake game</title>
+	<style>
+	#canvas{
+		background-color: black;
+	}
+</style>
+</head>
+<body>
+	<canvas id="canvas" width="500" height="500"></canvas>
+	<script src="snake.js"></script>
+</body>
+</html>
+	
